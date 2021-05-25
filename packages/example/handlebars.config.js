68e38e3ccd070a4893ed9a655d5f8f0e321cc1e6
andreyvolokitin <andreyvlktn@gmail.com>
2021-05-26 01:55:14 +0400

    Use actual package in the root (sorry)

--- packages/example/handlebars.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  data: 'src/data/**/*.{json,js}',
-  decorators: 'src/decorators/**/*.js',
-  helpers: 'src/helpers/**/*.js',
-  layouts: 'src/layouts/**/*.{hbs,handlebars,js}',
-  partials: 'src/partials/**/*.{hbs,handlebars,js}'
-}
