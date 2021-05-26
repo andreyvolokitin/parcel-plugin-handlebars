a61e5c96daa61e2132f6233db58d3a0eed104d38
andreyvolokitin <andreyvlktn@gmail.com>
2021-05-26 21:16:12 +0400

    Try to refactor to parcel v2

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
 /* -----------------------------------------------------------------------------
  * parcel-plugin-handlebars
  * -------------------------------------------------------------------------- */
-
-module.exports = bundler => {
-  bundler.addAssetType('hbs', require.resolve('./lib/handlebars-asset'))
-  bundler.addAssetType('handlebars', require.resolve('./lib/handlebars-asset'))
-  bundler.addAssetType('html', require.resolve('./lib/handlebars-asset'))
-}
+//
+// module.exports = bundler => {
+//   bundler.addAssetType('hbs', require.resolve('./lib/handlebars-asset'))
+//   bundler.addAssetType('handlebars', require.resolve('./lib/handlebars-asset'))
+//   bundler.addAssetType('html', require.resolve('./lib/handlebars-asset'))
+// }
+module.exports = require.resolve('./lib/handlebars-asset');
